e414adcc2012bf5c4d0b8e7e1018a758851e4754
Zack Driscoll <zacdrisc@uat.edu>
2020-01-27 12:12:24 -0700

    Start of Asteroids Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score = 0;
+    public bool isPaused = false;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("I tried to create a second Game Manager.");
+        }
+    }
+}
